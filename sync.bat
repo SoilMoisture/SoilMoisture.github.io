5928775f27fb0d53e7dde9ee31b9999e75eedbfc
SoilMoisture <31675164+SoilMoisture@users.noreply.github.com>
2018-06-03 09:28:53 -0600

    delete sync.bat

--- sync.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: add all new files 
-call git add . 
-call git commit -a -m "Automated commit on %fvar%"
-
-:: push to the server. Default in "origin" remote, "master" branch
-call git push
-exit
